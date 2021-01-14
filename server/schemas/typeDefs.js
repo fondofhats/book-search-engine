e5aaa74f3a5e599729deea369656c82096fbafe5
Michael Giddings <mikegiddings@gmail.com>
2021-01-13 19:56:35 -0500

    added apollo server and GraphQL helloWorld test

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,12 @@
+
+const { gql } = require('apollo-server-express');
+
+//create our typeDefs
+const typeDefs = gql`
+    type Query {
+        helloWorld: String
+    }
+`;
+
+//export the typeDefs
+module.exports = typeDefs;
